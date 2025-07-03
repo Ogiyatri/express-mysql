a56e7376d8a5da543d4c605cbbbf1242be9880d0
Malakiano <ogiyatrimalakiano@gmail.com>
2025-07-03 09:47:54 +0700

    feat: crud done

--- controller/users.js
+++ controller/users.js
@@ -6,43 +6,66 @@ const getAllUsers = async (req, res) => {
   // koneksi database gunakan try catch untuk menangani error
   try {
     const [data] = await usersModel.getAllUsers();
-  res.json({
-    message: "Daftar semua pengguna",
-    data: data,
-  });
-
+    res.json({
+      message: "Daftar semua pengguna",
+      data: data,
+    });
   } catch (error) {
     res.status(500).json({
-      message: "Server error",
+      message: "Get All UsersServer error",
       serverMessage: error,
-    })
+    });
   }
 };
 
-const createNewUser = (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: "Pengguna baru telah dibuat",
-    data: req.body,
-  });
+const createNewUser = async (req, res) => {
+  const { body } = req;
+
+  try {
+    await usersModel.createNewUser(body);
+    res.json({
+      message: "Pengguna baru telah dibuat",
+      data: req.body,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Create Users Server error",
+      serverMessage: error,
+    });
+  }
 };
 
 const updateUser = (req, res) => {
-    const { id } = req.params;
-    console.log(`Memperbarui pengguna dengan ID: ${id}`);
+  const { id } = req.params;
+
+  try {
+    usersModel.updateUser(id, req.body);
     res.json({
-        message: "Pengguna telah diperbarui",
-        data: req.body,
-    })
-}
+      message: "Pengguna telah diperbarui",
+      data: req.body,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Update Users Server error",
+      serverMessage: error,
+    });
+  }
+};
 
 const deleteUser = (req, res) => {
-    const { id } = req.params;
-    console.log(`Menghapus pengguna dengan ID: ${id}`);
+  const { id } = req.params;
+  try {
+    usersModel.deleteUser(id);
     res.json({
-        message: "Pengguna telah dihapus",
-        id: id,
+      message: "Pengguna telah dihapus",
+      id: id,
     });
+  } catch (error) {
+    res.status(500).json({
+      message: "Delete Users Server error",
+      serverMessage: error,
+    });
+  }
 };
 
 module.exports = {
--- models/users.js
+++ models/users.js
@@ -1,12 +1,35 @@
 const dbPool = require("../config/database");
 
 const getAllUsers = () => {
-    // pemanggil query ke dalam database berdasarkan tabel
-    const SQLQuery = 'SELECT * FROM users'
+  // pemanggil query ke dalam database berdasarkan tabel
+  const SQLQuery = "SELECT * FROM users";
 
-    return dbPool.execute(SQLQuery);
+  return dbPool.execute(SQLQuery);
+};
+
+const createNewUser = (body) => {
+  // pemanggil query ke dalam database berdasarkan tabel
+  const SQLQuery = `INSERT INTO users (name, email, address) 
+    VALUES ('${body.name}', '${body.email}', '${body.address}')`;
+
+  return dbPool.execute(SQLQuery);
+};
+
+const updateUser = (id, body) => {
+  const SQLQuery = `UPDATE users SET name = '${body.name}', email = '${body.email}', address = '${body.address}' WHERE id = ${id}`;
+
+  return dbPool.execute(SQLQuery);
+}
+
+const deleteUser = (id) => {
+  const SQLQuery = `DELETE FROM users WHERE id = ${id}`;
+
+  return dbPool.execute(SQLQuery);
 }
 
 module.exports = {
-    getAllUsers
-}
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser
+};
